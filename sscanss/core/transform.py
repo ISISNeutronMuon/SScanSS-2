Adds test for conversion between different rotation representations and few bug fixes

sscanss/core/transform.py
@@ -13,6 +13,7 @@ def angle_axis_to_matrix(angle, axis):
     :return: rotation matrix
     :rtype: pyrr.Matrix33
     """
+    _axis = axis.normalized
     c = math.cos(angle)
     s = math.sin(angle)
     t = 1 - c
@@ -20,19 +21,19 @@ def angle_axis_to_matrix(angle, axis):
     return Matrix33(
         [
             [
-                t * axis.x * axis.x + c,
-                t * axis.x * axis.y - axis.z * s,
-                t * axis.x * axis.z + axis.y * s,
+                t * _axis.x * _axis.x + c,
+                t * _axis.x * _axis.y - _axis.z * s,
+                t * _axis.x * _axis.z + _axis.y * s,
             ],
             [
-                t * axis.x * axis.y + axis.z * s,
-                t * axis.y * axis.y + c,
-                t * axis.y * axis.z - axis.x * s,
+                t * _axis.x * _axis.y + _axis.z * s,
+                t * _axis.y * _axis.y + c,
+                t * _axis.y * _axis.z - _axis.x * s,
             ],
             [
-                t * axis.x * axis.z - axis.y * s,
-                t * axis.y * axis.z + axis.x * s,
-                t * axis.z * axis.z + c,
+                t * _axis.x * _axis.z - _axis.y * s,
+                t * _axis.y * _axis.z + _axis.x * s,
+                t * _axis.z * _axis.z + c,
             ],
         ]
     )
@@ -47,18 +48,18 @@ def xyz_eulers_from_matrix(matrix):
     :return: XYZ Euler angles
     :rtype: pyrr.Vector3
     """
-    eps = 1e-7
-    yaw = math.asin(matrix.m13)
-
-    if matrix.m33 < 0:
-        yaw = math.pi - yaw if yaw >= 0 else -math.pi - yaw
-
-    if eps > matrix.m11 > -eps:
-        roll = 0.0
-        pitch = math.atan2(matrix.m21, matrix.m22)
-    else:
+    if 1 > matrix.m13 > -1:
+        yaw = math.asin(matrix.m13)
         roll = math.atan2(-matrix.m12, matrix.m11)
         pitch = math.atan2(-matrix.m23, matrix.m33)
+    elif matrix.m13 >= 1:
+        roll = 0.0
+        pitch = math.atan2(matrix.m21, matrix.m22)
+        yaw = math.pi/2
+    elif matrix.m13 <= -1:
+        roll = 0.0
+        pitch = -math.atan2(matrix.m21, matrix.m22)
+        yaw = -math.pi / 2
 
     return Vector3([pitch, yaw, roll])
 
@@ -72,16 +73,12 @@ def matrix_from_xyz_eulers(angles):
     :return: rotation matrix
     :rtype: pyrr.Matrix33
     """
-    x = angles[0]
-    y = angles[1]
-    z = angles[2]
-
-    sx = math.sin(x)
-    cx = math.cos(x)
-    sy = math.sin(y)
-    cy = math.cos(y)
-    sz = math.sin(z)
-    cz = math.cos(z)
+    sx = math.sin(angles.x)
+    cx = math.cos(angles.x)
+    sy = math.sin(angles.y)
+    cy = math.cos(angles.y)
+    sz = math.sin(angles.z)
+    cz = math.cos(angles.z)
 
     return Matrix33(np.array(
         [

sscanss/ui/commands/tools.py
@@ -1,4 +1,5 @@
 import numpy as np
+from pyrr import Vector3
 from PyQt5 import QtWidgets
 from sscanss.core.transform import matrix_from_xyz_eulers
 
@@ -6,7 +7,7 @@ from sscanss.core.transform import matrix_from_xyz_eulers
 class RotateSample(QtWidgets.QUndoCommand):
     def __init__(self, angles, sample_key, presenter):
         super().__init__()
-        self.angles = np.radians(angles)
+        self.angles = Vector3(np.radians(angles))
         self.key = sample_key
         self.model = presenter.model
 