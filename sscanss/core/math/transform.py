656ceda0664f358c4d258e19aa34aabdbb65d2a4
Stephen Nneji <steve.nneji@gmail.com>
2018-09-12 16:39:39 +0100

    Adds functions to compute rotation between vectors and mesh plane intersection

--- sscanss/core/math/transform.py
+++ sscanss/core/math/transform.py
@@ -3,6 +3,8 @@ import numpy as np
 from .vector import Vector3
 from .matrix import Matrix33
 
+eps = 0.000001
+
 
 def angle_axis_to_matrix(angle, axis):
     """ Converts rotation in angle/axis representation to a matrix
@@ -103,3 +105,56 @@ def matrix_from_xyz_eulers(angles):
             ]
         ]
     ))
+
+
+def rotation_btw_vectors(v1, v2):
+    """Creates a rotation matrices to rotate from one vector (v1) to another (v2)
+    based on Möller, Tomas, and John F. Hughes. "Efficiently building a matrix to rotate one vector to another."
+    Journal of graphics tools 4.4 (1999): 1-4.
+
+    :param v1: from vector
+    :type v1: Vector3
+    :param v2: to vector
+    :type v2:  Vector3
+    :return: rotation matrix
+    :rtype: Matrix33
+    """
+    v = np.cross(v1, v2)
+    e = np.dot(v1, v2)
+
+    m = Matrix33()
+    if abs(e) > (1.0 - eps):
+        # if the vector a reflections i.e. 180 degree
+        index = np.argmin(np.abs(v1))
+        x = np.zeros(3, dtype=np.float32)
+        x[index] = 1.0
+        u = x - v1
+        v = x - v2
+
+        c1 = 2.0 / np.dot(u, u)
+        c2 = 2.0 / np.dot(v, v)
+        c3 = c1 * c2 * np.dot(u, v)
+
+        m.m11 = 1 - c1 * u[0] * u[0] - c2 * v[0] * v[0] + c3 * v[0] * u[0]
+        m.m12 = - c1 * u[0] * u[1] - c2 * v[0] * v[1] + c3 * v[0] * u[1]
+        m.m13 = - c1 * u[0] * u[2] - c2 * v[0] * v[2] + c3 * v[0] * u[2]
+        m.m21 = - c1 * u[1] * u[0] - c2 * v[1] * v[0] + c3 * v[1] * u[0]
+        m.m22 = 1 - c1 * u[1] * u[1] - c2 * v[1] * v[1] + c3 * v[1] * u[1]
+        m.m23 = - c1 * u[1] * u[2] - c2 * v[1] * v[2] + c3 * v[1] * u[2]
+        m.m31 = - c1 * u[2] * u[0] - c2 * v[2] * v[0] + c3 * v[2] * u[0]
+        m.m32 = - c1 * u[2] * u[1] - c2 * v[2] * v[1] + c3 * v[2] * u[1]
+        m.m33 = 1 - c1 * u[2] * u[2] - c2 * v[2] * v[2] + c3 * v[2] * u[2]
+    else:
+        h = (1 - e) / np.dot(v, v)
+
+        m.m11 = e + h * v[0] * v[0]
+        m.m12 = h * v[0] * v[1] - v[2]
+        m.m13 = h * v[0] * v[2] + v[1]
+        m.m21 = h * v[0] * v[1] + v[2]
+        m.m22 = e + h * v[1] * v[1]
+        m.m23 = h * v[1] * v[2] - v[0]
+        m.m31 = h * v[0] * v[2] - v[1]
+        m.m32 = h * v[1] * v[2] + v[0]
+        m.m33 = e + h * v[2] * v[2]
+
+    return m
--- sscanss/core/mesh/geometry.py
+++ sscanss/core/mesh/geometry.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+eps = 0.000001
+
 
 def closest_point_on_triangle(vertex_a, vertex_b, vertex_c, point):
     a = vertex_a
@@ -62,3 +64,94 @@ def closest_triangle_to_point(vertices, point):
             min_dist = dist
 
     return closest_face, min_dist
+
+
+def mesh_plane_intersection(mesh, plane):
+    """Gets the intersection between a triangular mesh and a plane. The algorithm returns
+    a set of lines is points pairs where each even indexed point is the start of a line
+    and the next point is the end. An empty list implies no intersection.
+    Based on code from Real-Time Collision Detection (1st Edition) By Christer Ericson
+
+    :param mesh: a triangular mesh
+    :type mesh: sscanss.core.mesh.Mesh
+    :param plane: a plane
+    :type plane: sscanss.core.math.Plane
+    :return: array of points pairs
+    :rtype: list[numpy.ndarray]
+    """
+    # The algorithm checks if all vertices on each mesh face are on the same side of the plane
+    # if this is true, the face does not intersect the plane. Once the faces that intersect
+    # the plane are determined, the segments that form the face are tested for intersection with the plane.
+    segments = []
+    all_vertices = mesh  # get vertices
+    all_dist = np.dot(all_vertices - plane.point, plane.normal)
+    offsets = range(0, all_vertices.shape[0], 3)
+    gg = np.logical_and.reduceat(all_dist > 0, offsets)
+    ll = np.logical_and.reduceat(all_dist < 0, offsets)
+    indices = np.where(~ll & ~gg)[0] * 3
+    if indices.size == 0:
+        return segments
+
+    for index in indices:
+        dist = all_dist[index:index + 3]
+        vertices = all_vertices[index:index + 3, :]
+
+        points = []
+        zeros = np.nonzero(dist)[0]
+        if zeros.size == 0:
+            # all vertices on the plane we currently don't
+            # care about this condition
+            continue
+        elif zeros.size == 1:
+            # edge lies on the plane
+            a = vertices[zeros[0], :]
+            b = vertices[zeros[1], :]
+            segments.extend([a, b])
+            continue
+        elif zeros.size == 2:
+            # point lies on plane so check intersection for a single line
+            indices = (0, 1, 2)
+            points.append(vertices[zeros[0], :])
+            face_indices = [np.delete(indices, zeros[0])]
+        else:
+            face_indices = [(0, 1), (1, 2), (0, 2)]
+
+        for i, j in face_indices:
+            a = vertices[i, :]
+            b = vertices[j, :]
+
+            point = segment_plane_intersection(a, b, plane)
+            if point is not None:
+                points.append(point)
+
+            if len(points) == 2:
+                segments.extend([points[0], points[1]])
+                break
+
+    return segments
+
+
+def segment_plane_intersection(point_a, point_b, plane):
+    """Gets the intersection between a line segment and a plane
+
+    :param point_a:  3D starting point of the segment
+    :type point_a:numpy.ndarray
+    :param point_b: 3D ending point of the segment
+    :type point_b: numpy.ndarray
+    :param plane: the plane
+    :type plane: sscanss.core.math.Plane
+    :return: point of intersection or None if no intersection
+    :rtype: Union[numpy.ndarray, NoneType]
+    """
+    ab = point_b - point_a
+    n = - np.dot(plane.normal, point_a - plane.point)
+    d = np.dot(plane.normal, ab)
+    if -eps < d < eps:
+        # ignore case where line lies on plane
+        return None
+    t = n / d
+    if 0.0 <= t <= 1.0:
+        q = point_a + t * ab
+        return q
+
+    return None
