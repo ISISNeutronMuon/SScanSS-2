Modifies Euler angles used for sample rotation and alignment from XYZ to ZYX convention and fixes file dialog so its case-insensitive on Windows

sscanss/core/math/transform.py
@@ -173,6 +173,45 @@ def matrix_from_xyz_eulers(angles):
     ))
 
 
+def matrix_from_zyx_eulers(angles):
+    """Creates a rotation matrix from ZYX Euler angles
+
+    :param angles: ZYX Euler angles in radians
+    :type angles: Vector3
+    :return: rotation matrix
+    :rtype: Matrix33
+    """
+    sx = math.sin(angles[2])
+    cx = math.cos(angles[2])
+    sy = math.sin(angles[1])
+    cy = math.cos(angles[1])
+    sz = math.sin(angles[0])
+    cz = math.cos(angles[0])
+
+    return Matrix33(np.array(
+        [
+            # m1
+            [
+                cy * cz,
+                cz * sx * sy - cx * sz,
+                cx * cz * sy + sx * sz,
+            ],
+            # m2
+            [
+                cy * sz,
+                cx * cz + sx * sy * sz,
+                -cz * sx + cx * sy * sz,
+            ],
+            # m3
+            [
+                -sy,
+                cy * sx,
+                cx * cy,
+            ]
+        ]
+    ))
+
+
 def rotation_btw_vectors(v1, v2):
     """Creates a rotation matrices to rotate from one vector (v1) to another (v2).
     Both vectors are assumed to be normalized. The implementation is based on Möller,
@@ -231,14 +270,16 @@ def rotation_btw_vectors(v1, v2):
     return m
 
 
-def matrix_from_pose(pose, angles_in_degrees=True):
+def matrix_from_pose(pose, angles_in_degrees=True, order='xyz'):
     """Converts a 6D pose into a transformation matrix. Pose contains
-    3D translation (X, Y, Z) and 3D orientation (XYZ euler angles)
+    3D translation (X, Y, Z) and 3D orientation (XYZ or ZYX euler angles)
 
     :param pose: position and orientation
     :type pose: List[float]
     :param angles_in_degrees: indicates that angles are in degrees
     :type angles_in_degrees: bool
+    :param order: euler angle convention
+    :type order: str
     :return: transformation matrix
     :rtype: Matrix44
     """
@@ -246,8 +287,15 @@ def matrix_from_pose(pose, angles_in_degrees=True):
 
     position = pose[0:3]
     orientation = np.radians(pose[3:6]) if angles_in_degrees else pose[3:6]
+    order = order.lower()
+    if order == 'xyz':
+        matrix_from_euler_func = matrix_from_xyz_eulers
+    elif order == 'zyx':
+        matrix_from_euler_func = matrix_from_zyx_eulers
+    else:
+        raise ValueError(f'The given order {order} is not supported.')
 
-    matrix[0:3, 0:3] = matrix_from_xyz_eulers(orientation)
+    matrix[0:3, 0:3] = matrix_from_euler_func(orientation)
     matrix[0:3, 3] = position
 
     return matrix

tests/test_commands.py
@@ -73,18 +73,27 @@ class TestTransformCommands(unittest.TestCase):
         np.testing.assert_array_equal(sample['2'].indices, self.mesh_2.indices)
 
         # Command to rotate all the samples
-        angles = [30, 60, 90]
+        angles = [90, 60, 30]
         cmd = RotateSample(angles, None, self.presenter)
         cmd.redo()
 
         expected_vertices_1 = np.array([[1.59807621, -0.75, 3.29903811],
                                        [2.69615242, -0.20096189, 8.34807621],
                                        [3.79422863, 0.34807621, 13.39711432]])
+        expected_vertices_1 = np.array([[-0.2320508,  3.6160254,  0.9330127],
+                                       [-1.330127,  8.6650635,  0.3839746],
+                                       [-2.4282032, 13.7141016, -0.1650635]])
+
         expected_normals_1 = np.array([[0.866025, -0.25, 0.433013], [-0.5, -0.433013, 0.75], [0, 0.866025, 0.5]])
+        expected_normals_1 = np.array([[0.5, 0.75, 0.4330127], [-0.8660254, 0.4330127, 0.25], [0, 0.5, -0.8660254]])
         expected_vertices_2 = np.array([[3.79422863, 0.34807621, 13.39711432],
                                         [2.69615242, -0.20096189, 8.34807621],
                                         [1.59807621, -0.75, 3.29903811]])
+        expected_vertices_2 = np.array([[-2.4282032, 13.7141016, -0.1650635],
+                                       [-1.330127,  8.6650635,  0.3839746],
+                                       [-0.2320508,  3.6160254,  0.9330127]])
         expected_normals_2 = np.array([[-0.5, -0.433013, 0.75], [0.866025, -0.25, 0.433013], [0, 0.866025, 0.5]])
+        expected_normals_2 = np.array([[-0.8660254, 0.4330127, 0.25], [0.5, 0.75, 0.4330127], [0, 0.5, -0.8660254]])
         sample = self.model_mock.return_value.sample
         # Check that redo rotates vertices, normals but not the indices of all samples'
         np.testing.assert_array_almost_equal(sample['1'].vertices, expected_vertices_1, decimal=5)

tests/test_math.py
@@ -5,7 +5,7 @@ from sscanss.core.math import (Vector, Vector2, Vector3, Vector4, Matrix, Matrix
                                angle_axis_to_matrix, xyz_eulers_from_matrix, matrix_from_xyz_eulers,
                                Quaternion, QuaternionVectorPair, rigid_transform, find_3d_correspondence,
                                matrix_to_angle_axis, matrix_from_pose, rotation_btw_vectors, angle_axis_btw_vectors,
-                               fit_line_3d, fit_circle_3d, fit_circle_2d)
+                               fit_line_3d, fit_circle_3d, fit_circle_2d, matrix_from_zyx_eulers)
 
 
 class TestMath(unittest.TestCase):
@@ -58,10 +58,20 @@ class TestMath(unittest.TestCase):
         matrix = matrix_from_pose([-2.0, 5.0, 11.0, 0.0, np.pi/2, 0.0], angles_in_degrees=False)
         np.testing.assert_array_almost_equal(matrix, expected, decimal=5)
 
+        matrix = matrix_from_pose([-2.0, 5.0, 11.0, 0.0, np.pi/2, 0.0], angles_in_degrees=False, order='zyx')
+        np.testing.assert_array_almost_equal(matrix, expected, decimal=5)
+
         expected = Matrix44([[0.0, -0.707107, 0.707107, 12.0], [0.707107, 0.5, 0.5, 50.0],
                              [-0.707107, 0.5, 0.5, -3.0], [0.0, 0.0, 0.0, 1.0]])
         matrix = matrix_from_pose([12.0, 50.0, -3.0, -45.0, 45.0, 90.0])
         np.testing.assert_array_almost_equal(matrix, expected, decimal=5)
+
+        expected = Matrix44([[0.5, 0.5, -0.7071068, 12.0], [-0.5, -0.5, -0.7071068, 50.0],
+                             [-0.7071068, 0.7071068, 0.0, -3.0], [0.0, 0.0, 0.0, 1.0]])
+        matrix = matrix_from_pose([12.0, 50.0, -3.0, -45.0, 45.0, 90.0], order='zyx')
+        np.testing.assert_array_almost_equal(matrix, expected, decimal=5)
+
+        self.assertRaises(ValueError, matrix_from_pose, np.zeros(6), True, 'zzz')
 
     def testMatrixToAngleAxis(self):
         matrix = Matrix33.identity()
@@ -115,6 +125,24 @@ class TestMath(unittest.TestCase):
                             [0.7934120, 0.3578208, -0.4924039],
                             [0.3578208, 0.3802361, 0.8528686]])
         result = matrix_from_xyz_eulers(eulers)
+        np.testing.assert_array_almost_equal(result, expected, decimal=5)
+
+    def testMatrixFromZYXEulers(self):
+        eulers = Vector3([0.0, 0.0, 0.0])
+        expected = Matrix33.identity()
+        result = matrix_from_zyx_eulers(eulers)
+        np.testing.assert_array_almost_equal(result, expected, decimal=5)
+
+        eulers = Vector3(np.radians([90.0, 0.0, 0.0]))
+        expected = Matrix33([[0, -1, 0], [1, 0, 0], [0, 0, 1]])
+        result = matrix_from_zyx_eulers(eulers)
+        np.testing.assert_array_almost_equal(result, expected, decimal=5)
+
+        eulers = Vector3(np.radians([60.0, 10.0, 30.0]))
+        expected = Matrix33([[0.4924039, -0.7065880, 0.5082046],
+                             [0.8528686, 0.5082046, -0.1197639],
+                             [-0.1736482, 0.4924039, 0.8528686]])
+        result = matrix_from_zyx_eulers(eulers)
         np.testing.assert_array_almost_equal(result, expected, decimal=5)
 
     def testXYZEulersFromMatrix(self):

tests/test_ui.py
@@ -244,7 +244,7 @@ class TestMainWindow(unittest.TestCase):
         sample = list(self.model.sample.items())[0][1]
         np.testing.assert_array_almost_equal(sample.bounding_box.center, [0.0, 0.0, 0.0], decimal=5)
 
-        self.window.translate_sample_action.trigger()
+        QTest.mouseClick(self.toolbar.widgetForAction(self.window.translate_sample_action), Qt.LeftButton)
         widget = self.getDockedWidget(self.window.docks, TransformDialog.dock_flag)
         QTest.keyClick(widget.tool.y_position.form_lineedit, Qt.Key_A, Qt.ControlModifier)
         QTest.keyClick(widget.tool.y_position.form_lineedit, Qt.Key_Delete)
@@ -260,7 +260,7 @@ class TestMainWindow(unittest.TestCase):
         self.triggerRedo()
         np.testing.assert_array_almost_equal(sample.bounding_box.center, [0.0, 100.0, 0.0], decimal=5)
 
-        self.window.rotate_sample_action.trigger()
+        QTest.mouseClick(self.toolbar.widgetForAction(self.window.rotate_sample_action), Qt.LeftButton)
         widget = self.getDockedWidget(self.window.docks, TransformDialog.dock_flag)
         QTest.keyClick(widget.tool.z_rotation.form_lineedit, Qt.Key_A, Qt.ControlModifier)
         QTest.keyClick(widget.tool.z_rotation.form_lineedit, Qt.Key_Delete)
@@ -276,9 +276,9 @@ class TestMainWindow(unittest.TestCase):
         self.triggerRedo()
         np.testing.assert_array_almost_equal(sample.bounding_box.center, [-100.0, 0.0, 0.0], decimal=5)
 
-        self.window.transform_sample_action.trigger()
+        QTest.mouseClick(self.toolbar.widgetForAction(self.window.transform_sample_action), Qt.LeftButton)
 
-        self.window.move_origin_action.trigger()
+        QTest.mouseClick(self.toolbar.widgetForAction(self.window.move_origin_action), Qt.LeftButton)
         widget = self.getDockedWidget(self.window.docks, TransformDialog.dock_flag)
         for i in range(widget.tool.move_combobox.count()):
             widget.tool.move_combobox.setCurrentIndex(i)
@@ -291,7 +291,7 @@ class TestMainWindow(unittest.TestCase):
         np.testing.assert_array_almost_equal(sample.bounding_box.center, [-100., 0.0, 0.0], decimal=5)
         self.triggerRedo()
 
-        self.window.plane_align_action.trigger()
+        QTest.mouseClick(self.toolbar.widgetForAction(self.window.plane_align_action), Qt.LeftButton)
         widget = self.getDockedWidget(self.window.docks, TransformDialog.dock_flag)
         for i in range(widget.tool.plane_combobox.count()):
             widget.tool.plane_combobox.setCurrentIndex(i)