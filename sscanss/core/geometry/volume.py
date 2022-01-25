dede17511615ec8815ee4955e23565ef8926dc5b
jfkcooper <jos.cooper@stfc.ac.uk>
2022-01-25 10:41:02 +0000

    docstring format in Volume class

--- sscanss/core/geometry/volume.py
+++ sscanss/core/geometry/volume.py
@@ -4,8 +4,8 @@ Classes for Volume objects
 
 
 class Volume:
-    """Creates a Volume object. This is the result of loading in a tomography scan, either from a nexus file, or a set of TIFF files.
-     It is the equivalent of the mesh object but for tomography data
+    """Creates a Volume object. This is the result of loading in a tomography scan, either from a nexus file,
+    or a set of TIFF files. It is the equivalent of the mesh object but for tomography data
 
     :param data: N x M x L array of intensities, created by stacking L TIFF images, each of dimension N x M
     :type data: numpy.ndarray
