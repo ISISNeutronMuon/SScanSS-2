026dabe2aaa095b1b30705f00070074711e8d209
StephenNneji <34302892+StephenNneji@users.noreply.github.com>
2022-01-28 14:01:12 +0000

    wesfgs

--- sscanss/core/instrument/simulation.py
+++ sscanss/core/instrument/simulation.py
@@ -500,7 +500,7 @@ class Simulation(QtCore.QObject):
         self.timer.start()
 
     def checkResult(self):
-        """Checks for and notifies when result are availablebnbn dswawd"""
+        """Checks for and notifies when result are availablebnbn dswawd dfgsfgs"""
         queue = self.args['results']
         print('init size: ', queue.qsize())
         if not self.process.is_alive():
