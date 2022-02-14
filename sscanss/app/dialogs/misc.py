Displaying correct values but with race condition in instrument positioner (off by one position error I think)

sscanss/app/dialogs/misc.py
@@ -8,8 +8,8 @@ from matplotlib.figure import Figure
 from sscanss.config import path_for, __version__, settings
 from sscanss.core.instrument import IKSolver
 from sscanss.core.util import (DockFlag, Attributes, Accordion, Pane, create_tool_button, Banner, compact_path,
-                               StyledTabWidget, MessageType, PointType)
-from sscanss.app.widgets import AlignmentErrorModel, ErrorDetailModel, CenteredBoxProxy, PointModel
+                               StyledTabWidget, MessageType)
+from time import sleep
 
 
 class AboutDialog(QtWidgets.QDialog):
@@ -1384,14 +1384,16 @@ class CurrentCoordinatesDialog(QtWidgets.QDialog):
         self.parent = parent
         self.main_layout = QtWidgets.QVBoxLayout()
         self.setWindowTitle('Current Coordinates')
-        self.setMinimumSize(400, 300)
+        self.setMinimumSize(460, 300)
 
         self.tabs = QtWidgets.QTabWidget()
         self.fiducial_tab = FiducialsTab(self.parent)
         self.tabs.addTab(self.fiducial_tab, "Fiducials")
 
+        self.parent.presenter.model.fiducials_changed.connect(self.fiducial_tab.table_widget.update)
+
         self.matrix_tab = MatrixTab(self.parent)
-        self.tabs.addTab(self.matrix_tab, "Matrix")
+        self.tabs.addTab(self.matrix_tab, "Instrument Matrix")
 
         self.main_layout.addWidget(self.tabs)
         self.setLayout(self.main_layout)
@@ -1412,36 +1414,72 @@ class FiducialsTab(QtWidgets.QWidget):
         self.parent = parent
         self.main_layout = QtWidgets.QVBoxLayout()
 
-        self.table_model = QtWidgets.QTableWidget()
-        self.table_model.setColumnCount(3)
-        self.table_model.setRowCount(len(self.parent.presenter.model.fiducials['points']))
-        self.table_model.setShowGrid(True)
-        self.table_model.setHorizontalHeaderLabels(['X (mm)', 'Y (mm)', 'Z (mm)'])
-        self.table_model.
+        self.table_widget = QtWidgets.QTableWidget()
+        self.table_widget.setColumnCount(3)
+        self.table_widget.setShowGrid(True)
+        self.table_widget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        self.setData()
+        self.parent.presenter.model.fiducials_changed.connect(self.updateFiducials)
+        self.parent.presenter.model.instrument_controlled.connect(self.updateFiducials)
 
         self.export_button = QtWidgets.QPushButton(text='Export to file')
-        self.export_button.clicked.connect(self.export_points)
+        self.export_button.clicked.connect(self.exportPoints)
 
         self.main_layout.layout = QtWidgets.QVBoxLayout(self)
-        self.main_layout.layout.addWidget(self.table_model)
+        self.main_layout.layout.addWidget(self.table_widget)
         self.main_layout.layout.addWidget(self.export_button)
 
         self.setLayout(self.main_layout)
 
     def setData(self):
-        for n, key in enumerate(sorted(self.data.keys())):
-            horHeaders.append(key)
-            for m, item in enumerate(self.data[key]):
-                newitem = QTableWidgetItem(item)
-                self.setItem(m, n, newitem)
-        self.setHorizontalHeaderLabels(horHeaders)
+        """Sets the table header and inserts the data values into the cells"""
+        self.table_widget.setHorizontalHeaderLabels(['X (mm)', 'Y (mm)', 'Z (mm)'])
+        self.table_widget.setRowCount(len(self.parent.presenter.model.fiducials['points']))
 
-    def export_points(self):
+        fiducials_coordinates = self.fiducialToPosition()
+
+        for row, entry in enumerate(fiducials_coordinates):
+            for column in range(3):
+                fiducial_value = "{:.4f}".format(entry[column])
+                self.table_widget.setItem(row, column, QtWidgets.QTableWidgetItem(fiducial_value))
+
+    def fiducialToPosition(self):
+        """
+        :return: Fiducial coordinates in instrument frame
+        :rtype: np.ndarray
+        """
+        base_data = self.parent.presenter.model.fiducials
+        if self.parent.presenter.model.alignment is None:
+            self.parent.presenter.view.showMessage('Sample has not been aligned on instrument, co-ordinates are in sample co-ordinate frame.', MessageType.Warning)
+            fiducials_in_instrument_frame = base_data
+            return fiducials_in_instrument_frame
+
+        else:
+            '''fiducials_matrix = []
+            for i, fiducial in enumerate(base_data):
+                unrotated_pose = np.append(fiducial[0], [0, 0, 0])  # the three zeros indicate zero rotation
+                fiducials_matrix.append(matrix_from_pose(unrotated_pose))'''
+            pose = self.parent.presenter.model.instrument.positioning_stack.tool_pose
+            transform = pose @ self.parent.presenter.model.alignment
+            _matrix = transform[0:3, 0:3].transpose()
+            offset = transform[0:3, 3].transpose()
+
+            return base_data.points @ _matrix + offset
+
+
+
+    def updateFiducials(self):
+        """Updates model's data and layout"""
+        self.table_widget.clear()
+        self.setData()
+
+    def exportPoints(self):
+        """Writes out the data to a .fpos file if the sample has been aligned on the instrument"""
         if self.parent.presenter.model.alignment is None:
             self.parent.presenter.view.showMessage('Sample has not been aligned on instrument.', MessageType.Warning)
             return
         else:
-            name, _ = QtWidgets.QFileDialog.getSaveFileName(self, 'Save Fudicials', '', "fiducial file (*.fiducial)")
+            name, _ = QtWidgets.QFileDialog.getSaveFileName(self, 'Save Fudicials', '', "fiducial file (*.fpos)")
             if not name:
                 return
             else:
@@ -1461,26 +1499,38 @@ class MatrixTab(QtWidgets.QWidget):
         self.parent = parent
         self.main_layout = QtWidgets.QVBoxLayout()
 
-        self.table_model = QtWidgets.QTableWidget()
-        self.table_model.setColumnCount(4)
-        self.table_model.setShowGrid(True)
+        self.table_widget = QtWidgets.QTableWidget()
+        self.table_widget.setColumnCount(4)
+        self.table_widget.setRowCount(4)
+        self.table_widget.setShowGrid(True)
+        self.table_widget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        self.setData()
+        self.parent.presenter.model.instrument_controlled.connect(self.updatePose)
 
         self.export_button = QtWidgets.QPushButton(text='Export to file')
-        self.export_button.clicked.connect(self.export_points)
+        self.export_button.clicked.connect(self.exportPoints)
 
         self.main_layout.layout = QtWidgets.QVBoxLayout(self)
-        self.main_layout.layout.addWidget(self.table_model)
+        self.main_layout.layout.addWidget(self.table_widget)
         self.main_layout.layout.addWidget(self.export_button)
 
         self.setLayout(self.main_layout)
 
-    def export_points(self):
-        if self.parent.presenter.model.alignment is None:
-            self.parent.presenter.view.showMessage('Sample has not been aligned on instrument.', MessageType.Warning)
+    def setData(self):
+        for row, entry in enumerate(self.parent.presenter.model.instrument.positioning_stack.pose):
+            for column in range(4):
+                pose_value = "{:.4f}".format(entry[column])
+                self.table_widget.setItem(row, column, QtWidgets.QTableWidgetItem(pose_value))
+
+    def updatePose(self):
+        """Updates model's data and layout"""
+        self.table_widget.clear()
+        self.setData()
+
+    def exportPoints(self):
+        """Writes out the data to a .mat file"""
+        name, _ = QtWidgets.QFileDialog.getSaveFileName(self, 'Save pose matrix', '', "matrix file (*.mat)")
+        if not name:
             return
         else:
-            name, _ = QtWidgets.QFileDialog.getSaveFileName(self, 'Save pose matrix', '', "matrix file (*.mat)")
-            if not name:
-                return
-            else:
-                np.savetxt(name, self.parent.presenter.model.instrument.positioning_stack.pose, fmt='%.5f')
+            np.savetxt(name, self.parent.presenter.model.instrument.positioning_stack.pose, fmt='%.5f')