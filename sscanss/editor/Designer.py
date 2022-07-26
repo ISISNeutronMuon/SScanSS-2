633fb213bae2eafccef22e85b1235359cbd7e077
TimofeiZavileiskii <timofey.zavileyskiy@gmail.com>
2022-07-26 18:11:41 +0100

    Temp commit

--- sscanss/editor/Designer.py
+++ sscanss/editor/Designer.py
@@ -1,10 +1,15 @@
 from PyQt5 import QtCore, QtWidgets
-import InstrumentModel as im
 from functools import partial
+from sscanss.core.instrument.robotics import Link
+import json
+import InstrumentModel as im
 
 class ObjectStack(QtWidgets.QWidget):
+    """Holds the stack of selected objects to allow the user navigate the Json file and edit nested objects
+    :param parent: parent widget
+    :type parent: QWidget
+    """
     stackChanged = QtCore.pyqtSignal()
-
     def __init__(self, parent):
         super().__init__(parent)
 
@@ -19,35 +24,62 @@ class ObjectStack(QtWidgets.QWidget):
             self.layout.itemAt(i).widget().setParent(None)
 
     def createUi(self):
+        """Recreates the UI based on the contents of the stack, creating a button for each object
+        there to allow the user to return to any of the previous objects
+        """
         self.clearLayout()
         first = True
         for title, object in self.object_stack:
             if first:
                 first = False
             else:
-                self.layout.addWidget(QtWidgets.QLabel(">"))
+                label = QtWidgets.QLabel(">")
+                label.setMaximumSize(10, 50)
+                #self.layout.addWidget(label)
 
-            button = QtWidgets.QPushButton()
-            button.setText(title)
+            button = QtWidgets.QPushButton(title)
             button.clicked.connect(partial(self.goDown, object))
-            self.layout.addWidget(button)
+            button.setMaximumSize(100, 50)
+            self.layout.addWidget(button, 0, QtCore.Qt.AlignLeft)
+
+        for i in reversed(range(self.layout.count())):
+            print(self.layout.itemAt(i).widget())
 
     def addObject(self, object_title, new_object):
+        """Adds another object on top of the stack and updates UI accordingly
+        :param object_title: title of the object to be shown in the button
+        :type object_title: str
+        :param new_object: the object to be added
+        :type new_object: JsonObjectAttribute
+        """
         self.object_stack.append((object_title, new_object))
         self.stackChanged.emit()
 
     def goDown(self, selected_object):
+        """Goes down the stack removing all objects until the selected one
+        :param selected_object: Object on top of which everything will be removed
+        :type selected_object: JsonObjectAttribute
+        """
         while self.top() != selected_object:
             self.object_stack.pop(-1)
 
         self.stackChanged.emit()
 
     def top(self):
+        """Returns the stack on top of the stack - it is supposed to be currently active
+        :return selected_object: the top object
+        :rtype selected_object: JsonObjectAttribute
+        """
         title, obj = self.object_stack[-1]
         return obj
 
 class Designer(QtWidgets.QWidget):
-    def __init__(self, parent, instrument_model):
+    def __init__(self, parent):
+        """Creates an instance of the designer widget to edit a Json file with a GUI
+        :param parent: instance of the main window
+        :type parent: MainWindow
+        """
+
         super().__init__(parent)
 
         self.object_stack = ObjectStack(self)
@@ -63,55 +95,99 @@ class Designer(QtWidgets.QWidget):
 
         self.object_stack.addObject("instrument", self.instrument_model)
 
-    def createSchema(self):
-        detectorObjectAttr = {"name": im.JsonString("Name1"),
-                              "default_collimator": im.JsonString(),
-                              "diffracted_beam": im.JsonString(),
-                              "positioner": im.JsonString()}
-
-        detectorObject = im.JsonObject(detectorObjectAttr, self.object_stack)
-
-        detectorObjectAttr2 = {"name": im.JsonString("Name2"),
-                               "default_collimator": im.JsonString(),
-                               "diffracted_beam": im.JsonString(),
-                               "positioner": im.JsonString()}
-
-        detectorObject2 = im.JsonObject(detectorObjectAttr2, self.object_stack)
-
+    def createVisualObject(self):
         visual_object_attr = {"pose": im.JsonAttributeArray([im.JsonFloat(), im.JsonFloat(), im.JsonFloat()]),
                               "colour": im.JsonColour(),
-                              "mesh": im.JsonFile(self)}
+                              "mesh": im.JsonFile()}
 
         visual_object = im.JsonDirectlyEditableObject(visual_object_attr, self.object_stack)
+        return visual_object
 
-        jaws_object_attr = {"name": im.JsonString("Name2"),
-                            "aperture": im.JsonAttributeArray([im.JsonFloat(), im.JsonFloat(), im.JsonFloat()]),
-                            "aperture_lower_limit": im.JsonAttributeArray([im.JsonFloat(), im.JsonFloat(), im.JsonFloat()]),
-                            "aperture_upper_limit": im.JsonAttributeArray([im.JsonFloat(), im.JsonFloat(), im.JsonFloat()]),
-                            "visual": visual_object
-                            }
+    def createAttributeArray(self, type, number):
+        return im.JsonAttributeArray([type.defaultCopy() for i in range(number)])
 
+    def addCyclicReferences(self, arr1, arr2, key1, key2):
+        arr1.objects[0].attributes[key1] = im.JsonObjReference(arr2)
+        arr2.objects[0].attributes[key2] = im.JsonObjReference(arr1)
+
+    def createSchema(self):
+        key = "name"
+        fixed_hardware_attr = {key: im.JsonString("Name1"),
+                               "visual": self.createVisualObject()}
+        fixed_hardware_arr = im.JsonObjectArray([im.JsonObject(fixed_hardware_attr,
+                                                 self.object_stack)], key, self.object_stack)
+
+        link_object_attr = {key: im.JsonString("Name1"),
+                            "visual": self.createVisualObject()}
+        link_obj_arr = im.JsonObjectArray([im.JsonObject(link_object_attr, self.object_stack)], key, self.object_stack)
+
+        joint_object_attr = {key: im.JsonString("Name1"),
+                             "type": im.JsonEnum(Link.Type),
+                             "parent": im.JsonObjReference(link_obj_arr),
+                             "axis": self.createAttributeArray(im.JsonFloat(), 3),
+                             "origin": self.createAttributeArray(im.JsonFloat(), 3),
+                             "lower_limit": im.JsonFloat(),
+                             "upper_limit": im.JsonFloat(),
+                             "home_offset": im.JsonFloat()}
+        joint_arr = im.JsonObjectArray([im.JsonObject(joint_object_attr, self.object_stack)], key, self.object_stack)
+
+        positioner_attr = {key: im.JsonString("Name1"),
+                           "base": self.createAttributeArray(im.JsonFloat(), 6),
+                           "tool": self.createAttributeArray(im.JsonFloat(), 6),
+                           "custom_order": self.createAttributeArray(im.JsonString(), 6),
+                           "joints": im.JsonObjReference(joint_arr),
+                           "links": im.JsonObjReference(link_obj_arr)}
+
+        positioner_arr = im.JsonObjectArray([im.JsonObject(positioner_attr, self.object_stack)], key, self.object_stack)
+
+        jaws_object_attr = {key: im.JsonString("Name2"),
+                            "aperture": self.createAttributeArray(im.JsonFloat(), 3),
+                            "aperture_lower_limit": self.createAttributeArray(im.JsonFloat(), 3),
+                            "aperture_upper_limit": self.createAttributeArray(im.JsonFloat(), 3),
+                            "beam_source": self.createAttributeArray(im.JsonFloat(), 3),
+                            "positioner": im.JsonObjReference(positioner_arr),
+                            "visual": self.createVisualObject()}
         jaws_object = im.JsonObject(jaws_object_attr, self.object_stack)
 
-        detectorsArray = im.JsonObjectArray([detectorObject, detectorObject2], "name", self.object_stack)
+        detector_object_attr = {key: im.JsonString("Name1"),
+                                "default_collimator": None,
+                                "diffracted_beam": self.createAttributeArray(im.JsonFloat(), 3),
+                                "positioner": im.JsonObjReference(positioner_arr)}
+        detector_arr = im.JsonObjectArray([im.JsonObject(detector_object_attr, self.object_stack)],
+                                          key, self.object_stack)
 
-        instrumentClassAttr = {"name": im.JsonString(),
-                               "version": im.JsonObjReference(detectorsArray),
-                               "script_template": im.JsonString(),
-                               "gauge_volume": im.JsonAttributeArray([im.JsonFloat(), im.JsonFloat(), im.JsonFloat()]),
-                               "incident_jaws": jaws_object,
-                               "detectors": detectorsArray,
-                               "collimators": im.JsonObjectArray([], "name", self.object_stack),
-                               "positioning_stacks": im.JsonObjectArray([], "name", self.object_stack),
-                               "positioners": im.JsonObjectArray([], "name", self.object_stack),
-                               "fixed_hardware": im.JsonObjectArray([], "name", self.object_stack),
-                               }
+        collimator_object_attr = {key: im.JsonString("Name1"),
+                                  "detector": None,
+                                  "aperture": self.createAttributeArray(im.JsonFloat(), 2),
+                                  "visual": self.createVisualObject()}
+        collimator_arr = im.JsonObjectArray([im.JsonObject(collimator_object_attr, self.object_stack)],
+                                            key, self.object_stack)
 
-        instrument_model = im.JsonObject(instrumentClassAttr, self.object_stack)
+        self.addCyclicReferences(detector_arr, collimator_arr, "default_collimator", "detector")
+
+        instrument_class_attr = {"name": im.JsonString(),
+                                 "version": im.JsonString(),
+                                 "script_template": im.JsonFile(),
+                                 "gauge_volume": im.JsonAttributeArray([im.JsonFloat(), im.JsonFloat(), im.JsonFloat()]),
+                                 "incident_jaws": jaws_object,
+                                 "detectors": detector_arr,
+                                 "collimators": collimator_arr,
+                                 "positioning_stacks": im.JsonObjectArray([], key, self.object_stack),
+                                 "positioners": positioner_arr,
+                                 "fixed_hardware": fixed_hardware_arr}
+
+        instrument_model = im.JsonObject(instrument_class_attr, self.object_stack)
 
         return instrument_model
 
+    def getJsonFile(self):
+        return self.instrument_model.getJsonValue()
+
+    def setJsonFile(self, text):
+        pass
+
     def createUi(self):
+        """Updates the UI according to the top object in the stack"""
         self.attributes_panel.setParent(None)
         self.attributes_panel = self.object_stack.top().createPanel()
         self.layout.addWidget(self.attributes_panel)
--- sscanss/editor/InstrumentModel.py
+++ sscanss/editor/InstrumentModel.py
@@ -1,20 +1,42 @@
 from functools import partial
 from PyQt5 import QtWidgets, QtCore, QtGui
-from sscanss.core.util.widgets import FileDialog, FilePicker, ColourPicker
+from sscanss.core.util.widgets import FilePicker, ColourPicker
 
 
 class JsonAttribute(QtCore.QObject):
     def __init__(self):
+        """The parent class of all the nodes in the tree
+        Contains the methods which should be overriden by the child classes
+        """
         super().__init__()
 
     def createWidget(self):
+        """Creates the widget which should be used in the GUI to edit the data.
+        It is linked with the object using events
+        """
         pass
 
     def defaultCopy(self):
+        """Creates a copy of the node, it is needed to be able to create an object,
+        following the same schema as another object
+        """
         return type(self)()
 
+    def setJsonValue(self, json_value):
+        """Sets the value in the attribute from the value passed from a Json file"""
+        self.value = json_value
+
+    def getJsonValue(self):
+        """Returns the value from the attribute, suitable for use in a Json file"""
+        return self.value
 
 class JsonVariable(JsonAttribute):
+    """The parent class of all leafs of the tree. It represents single
+    attributes which contain one value and should be edited directly by created widget.
+    Have the has_changed event which should be triggered every time value is set
+    :param value: initial value of the attribute
+    :type value: object
+    """
     has_changed = QtCore.pyqtSignal(object)
 
     def __init__(self, value=None):
@@ -27,10 +49,18 @@ class JsonVariable(JsonAttribute):
 
 
 class JsonString(JsonVariable):
+    """Attribute which contains a simple string
+    :param value: initial string
+    :type value: str
+    """
     def __init__(self, value=''):
         super().__init__(value)
 
     def createWidget(self, title=''):
+        """Creates a line edit to modify the string in the attribute
+        :return: the line edit
+        :rtype: QLineEdit
+        """
         widget = QtWidgets.QLineEdit()
         widget.setText(self.value)
         widget.textEdited.connect(self.setValue)
@@ -38,33 +68,35 @@ class JsonString(JsonVariable):
 
 
 class JsonFile(JsonVariable):
-    def __init__(self, parent, value=''):
+    """Attribute which should allow to modify a file path
+    :param directory: the initial directory which will be suggested to a user
+    :type directory: str
+    :param filter: which files should be allowed to be open
+    :type filter: str
+    :param value: the initial address in the attribute
+    :type value: str
+    """
+    def __init__(self, directory='', filter='', value=''):
         super().__init__(value)
-        self.parent = parent
-
-    def pickFile(self):
-        dialog = FileDialog(self.parent, "Open dir", "", "")
-        self.value = dialog.getOpenFileName(self.parent, "Open dir", "", "")
-        self.label.setText(self.value)
+        self.directory = directory
+        self.filter = filter
 
     def createWidget(self, title=''):
-        widget = FilePicker("", "", "")
+        """Creates the file picker to choose the filepath in the attribute
+        :return: the file picker
+        :rtype: FilePicker
+        """
+        widget = FilePicker(self.directory, False, self.filter)
+        widget.value = self.value
         widget.value_changed.connect(self.setValue)
-        """
-        widget = QtWidgets.QWidget()
-        widget.layout = QtWidgets.QHBoxLayout()
-        widget.setLayout(widget.layout)
-        self.label = QtWidgets.QLabel(" "*160)
-        button = QtWidgets.QPushButton()
-        button.setText("Pick file...")
-        button.clicked.connect(self.pickFile)
-        widget.layout.addWidget(self.label)
-        widget.layout.addWidget(button)
-        """
+
         return widget
 
+    def defaultCopy(self):
+        return JsonFile(self.directory, self.filter)
 
 class JsonFloat(JsonVariable):
+    """Attribute manages the float values in objects"""
     def __init__(self, value=0.0):
         super().__init__(value)
 
@@ -72,13 +104,19 @@ class JsonFloat(JsonVariable):
         self.value = float(new_float)
 
     def createWidget(self, title=''):
+        """Creates a spin box to enter a float value
+        :return: The spin box
+        :rtype: QDoubleSpinBox
+        """
         widget = QtWidgets.QDoubleSpinBox()
+        widget.setRange(float("-inf"), float("inf"))
         widget.setValue(self.value)
         widget.valueChanged.connect(self.setValue)
         return widget
 
 
 class JsonAttributeArray(JsonAttribute):
+    """Array of several attributes allowing to edit them on the same line"""
     def __init__(self, attributes):
         super().__init__()
         self.attributes = attributes
@@ -99,45 +137,90 @@ class JsonAttributeArray(JsonAttribute):
 
         return list_widget
 
+    def defaultCopy(self):
+        return JsonAttributeArray([attribute.defaultCopy() for attribute in self.attributes])
+
+    def getJsonValue(self):
+        return [attr.getJsonValue() for attr in self.attributes]
 
 class JsonColour(JsonVariable):
+    rgbSize = 255
+
     def __init__(self, value=QtGui.QColor()):
         super().__init__(value)
 
     def setValue(self, new_value):
-        self.value = QtGui.QColor(new_value[0] * 255, new_value[1] * 255, new_value[2] * 255, new_value[3] * 255)
+        self.value = QtGui.QColor(new_value[0] * self.rgbSize, new_value[1] * self.rgbSize, new_value[2] * self.rgbSize, new_value[3] * self.rgbSize)
 
     def createWidget(self, title=''):
         widget = ColourPicker(self.value)
-        widget.value_changed.connect(self.setValue) #Check the colour is actually picked
+        widget.value_changed.connect(self.setValue)
         return widget
+
+    def getJsonValue(self):
+        return [self.value.value(0) / self.rgbSize, self.value.value(0) / self.rgbSize, self.value.value(0) / self.rgbSize]
 
 
 class JsonObjReference(JsonVariable):
-    def __init__(self, object_array, value=''):
+    """Attribute which contains name of an object from an object array
+    """
+    def __init__(self, object_array, value=0):
         super().__init__(value)
         self.object_array = object_array
 
-    def setValue(self, new_value):
-        self.value = self.combo_box.itemText(new_value)
-
-    def createWidget(self, title = ''):
+    def createWidget(self, title=''):
+        """Creates a combobox to choose one object from already existing ones in the list
+        :return: the combobox where the user can select the object
+        :rtype: QComboBox
+        """
         self.combo_box = QtWidgets.QComboBox()
         self.combo_box.addItems(self.object_array.getObjectKeys())
+        self.combo_box.setCurrentIndex(self.value)
         self.combo_box.currentIndexChanged.connect(self.setValue)
 
         return self.combo_box
 
+    def defaultCopy(self):
+        return JsonObjReference(self.object_array)
+
+    def getJsonValue(self):
+        return self.object_array.getObjectKeys()[self.value]
 
 class JsonEnum(JsonVariable):
-    def __init__(self, value=0):
+    def __init__(self, enum_class, value=0):
         super().__init__(value)
+        self.enum = enum_class
 
-    def createWidget(self):
-        pass
+    def enumList(self):
+        return [option.name for option in self.enum]
+
+    def createWidget(self, title=''):
+        widget = QtWidgets.QComboBox()
+        widget.addItems(self.enumList())
+        widget.setCurrentIndex(self.value)
+        widget.currentIndexChanged.connect(self.setValue)
+
+        return widget
+
+    def defaultCopy(self):
+        return JsonEnum(self.enum)
+
+    def getJsonValue(self):
+        return self.enumList()[self.value]
+
+class ObjectOrder(JsonVariable):
+    def __init__(self, object_array, value=''):
+        super().__init__()
+        self.object_array = object_array
+        self.value = value
 
 
 class JsonObjectAttribute(JsonVariable):
+    """Parent class of all the node attributes - classes should be able to be added to the object stack and
+    allow to modify their attributes by getting their widgets
+    :param object_stack: the reference to the object stack from the designer widget
+    :type object_stack: ObjectStack
+    """
     def __init__(self, object_stack):
         super().__init__()
         self.object_stack = object_stack
@@ -146,12 +229,13 @@ class JsonObjectAttribute(JsonVariable):
         return ' '.join([word.capitalize() for word in string.split('_')])
 
     def createWidget(self, title=''):
-        button = QtWidgets.QPushButton()
-        button.setText("Edit " + self.formatTitle(title) + "...")
-        button.clicked.connect(partial(self.object_stack.addObject, self.formatTitle(title), self))  #!-! Storing state in the gui feels bad
+        title = self.formatTitle(title)
+        button = QtWidgets.QPushButton("Edit " + title + "...")
+        button.clicked.connect(partial(self.object_stack.addObject, title, self))
         return button
 
     def createPanel(self):
+        """"""
         pass
 
 
@@ -170,38 +254,46 @@ class JsonObjectArray(JsonObjectAttribute):
     def selected(self):
         return self.objects[self.current_index]
 
+    @selected.setter
+    def selected(self, new_obj):
+        self.objects[self.current_index] = new_obj
+
+    @property
+    def prototype(self):
+        return self.objects[0]
+
     def newSelection(self, new_index):
         self.current_index = new_index
-        self.swapSelectedObject()
+        self.updateSelectedPanel()
 
-    def swapSelectedObject(self):
+    def updateSelectedPanel(self):
         self.panel.layout.itemAtPosition(1, 0).widget().setParent(None)
         self.panel.layout.addWidget(self.selected.createPanel(), 1, 0)
 
     def newObject(self):
-        self.objects.append(self.objects[0].defaultCopy())
+        self.objects.append(self.prototype.defaultCopy())
         self.current_index = len(self.objects) - 1
         self.selected.attributes[self.key_attribute].has_changed.connect(self.updateComboBox)
-        self.swapSelectedObject()
+        self.updateSelectedPanel()
         self.updateComboBox()
 
     def getObjectKeys(self):
         return [obj.attributes[self.key_attribute].value for obj in self.objects]
 
     def deleteObject(self):
-        if(len(self.objects) == 1):
-            self.objects[0] = self.selected.defaultCopy()
-            self.objects[0].attributes[self.key_attribute].has_changed.connect(self.updateComboBox)
+        if len(self.objects) == 1:
+            self.selected = self.prototype.defaultCopy()
+            self.selected.attributes[self.key_attribute].has_changed.connect(self.updateComboBox)
         else:
             self.objects.pop(self.current_index)
-            if(self.current_index > 1):
+            if self.current_index > 0:
                 self.current_index -= 1
-        self.swapSelectedObject()
+        self.updateSelectedPanel()
         self.updateComboBox()
 
     def moveObject(self):
         moved_object = self.objects.pop(self.current_index)
-        if(self.current_index < len(self.objects) - 1):
+        if self.current_index < len(self.objects):
             self.current_index += 1
         self.objects.insert(self.current_index, moved_object)
         self.updateComboBox()
@@ -223,6 +315,9 @@ class JsonObjectArray(JsonObjectAttribute):
         self.panel.setLayout(self.panel.layout)
         self.updateComboBox()
 
+        button_widget = QtWidgets.QWidget()
+        button_widget.layout = QtWidgets.QVBoxLayout()
+        button_widget.setLayout(button_widget.layout)
         add_button = QtWidgets.QPushButton()
         add_button.setText("Add")
         add_button.clicked.connect(self.newObject)
@@ -232,15 +327,22 @@ class JsonObjectArray(JsonObjectAttribute):
         swap_button = QtWidgets.QPushButton()
         swap_button.setText("Swap")
         swap_button.clicked.connect(self.moveObject)
+        button_widget.layout.addWidget(add_button)
+        button_widget.layout.addWidget(delete_button)
+        button_widget.layout.addWidget(swap_button)
 
-        self.panel.layout.addWidget(add_button, 0, 1)
-        self.panel.layout.addWidget(delete_button, 1, 1)
-        self.panel.layout.addWidget(swap_button, 2, 1)
+        self.panel.layout.addWidget(button_widget, 0, 1, 2, 1)
         self.panel.layout.addWidget(self.selected.createPanel(), 1, 0)
+        self.panel.layout.setRowStretch(3, 0)
         self.panel.layout.setRowStretch(3, 1)
 
         return self.panel
 
+    def defaultCopy(self):
+        return JsonObjectArray([self.prototype.defaultCopy()], self.key_attribute, self.object_stack)
+
+    def getJsonValue(self):
+        return [obj.getJsonValue() for obj in self.objects]
 
 class JsonObject(JsonObjectAttribute):
     def __init__(self, attributes, object_stack):
@@ -266,7 +368,15 @@ class JsonObject(JsonObjectAttribute):
             new_attributes[key] = attribute.defaultCopy()
         return JsonObject(new_attributes, self.object_stack)
 
+    def getJsonValue(self):
+        return {title: value.getJsonValue() for (title, value) in self.attributes}
 
 class JsonDirectlyEditableObject(JsonObject):
     def createWidget(self, title=''):
         return self.createPanel()
+
+    def defaultCopy(self):
+        new_attributes = {}
+        for key, attribute in self.attributes.items():
+            new_attributes[key] = attribute.defaultCopy()
+        return JsonDirectlyEditableObject(new_attributes, self.object_stack)
