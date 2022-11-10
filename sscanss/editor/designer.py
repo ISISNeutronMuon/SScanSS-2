53d789451c07339c918bf7569be6429d0028debd
PaulSharp <paul.sharp@stfc.ac.uk>
2022-11-10 16:27:48 +0000

    Added new method "editor.designer.PositioningStacksComponent.value()"

--- sscanss/editor/designer.py
+++ sscanss/editor/designer.py
@@ -1582,3 +1582,31 @@ class PositioningStacksComponent(QtWidgets.QWidget):
         self.positioners_combobox.clear()
         self.positioners_combobox.addItems([*positioners])
         self.positioners_combobox.setCurrentIndex(index)
+
+    def value(self):
+        """Returns the updated json from the component's inputs
+
+        :return: updated instrument json
+        :rtype: Dict[str, Any]
+        """
+        json_data = {}
+
+        name = self.name_combobox.currentText()
+        if name:
+            json_data['name'] = name
+
+        positioners = []
+        for index in self.positioning_stack_box.count():
+            positioners.append(self.positioning_stack_box.item(index))
+
+        if positioners:
+            json_data['positioners'] = positioners
+
+        # Place edited positioning stack within the list of positioning stacks
+        try:
+            self.positioning_stack_list[self.name_combobox.currentIndex()] = json_data
+        except IndexError:
+            self.positioning_stack_list.append(json_data)
+
+        # Return updated set of positioning stacks
+        return {self.key: self.positioning_stack_list}
