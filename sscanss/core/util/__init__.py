3963780a8dbe0c11db445b92f1392d097de92222
RobBuchananCompPhys <robertbb269@hotmail.com>
2023-09-06 10:49:40 +0100

    reformatted

--- sscanss/core/util/__init__.py
+++ sscanss/core/util/__init__.py
@@ -1,6 +1,6 @@
-from .misc import (Directions, Primitives, VisualGeometry, to_float, TransformType, DockFlag, PointType, Attributes, POINT_DTYPE,
-                   StrainComponents, LoadVector, MessageType, CommandID, toggle_action_in_group, PlaneOptions,
-                   compact_path, find_duplicates, MessageReplyType, InsertSampleOptions, Anchor)
+from .misc import (Directions, Primitives, VisualGeometry, to_float, TransformType, DockFlag, PointType, Attributes,
+                   POINT_DTYPE, StrainComponents, LoadVector, MessageType, CommandID, toggle_action_in_group,
+                   PlaneOptions, compact_path, find_duplicates, MessageReplyType, InsertSampleOptions, Anchor)
 from .worker import Worker, ProgressReport
 from .forms import FormGroup, FormControl, CompareValidator, Banner, FormTitle
 from .widgets import (create_icon, create_header, create_scroll_area, create_tool_button, FileDialog, FilePicker,
