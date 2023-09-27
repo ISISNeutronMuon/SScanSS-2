cbc75280f518ce5ad2dae5ee4c1132aa412d74ca
RobBuchananCompPhys <robertbb269@hotmail.com>
2023-09-27 10:40:12 +0100

    refactored read visuals function

--- sscanss/core/instrument/create.py
+++ sscanss/core/instrument/create.py
@@ -215,11 +215,11 @@ def read_visuals(visuals_data, path=''):
     else the specified primitive geometry is used
 
     :param visuals_data: visuals description
-    :type visuals_data: Union[Dict, None]
+    :type visuals_data: Optional[Dict]
     :param path: directory of the instrument description file
     :type path: str
     :return: mesh
-    :rtype: Union[Mesh, None]
+    :rtype: Optional[Mesh]
     """
     if visuals_data:
         pose = visuals_data.get('pose', DEFAULT_POSE)
@@ -227,57 +227,23 @@ def read_visuals(visuals_data, path=''):
         mesh_colour = visuals_data.get('colour', DEFAULT_COLOUR)
 
         if visuals_data.get('mesh'):
-            mesh = read_3d_model(pathlib.Path(path).joinpath(visuals_data.get('mesh', '')).as_posix())
-
-        if visuals_data.get('geometry'):
+            mesh = read_3d_model(pathlib.Path(path).joinpath(visuals_data['mesh']).as_posix())
+        else:
             geometry_data = visuals_data['geometry']
-
-            if not geometry_data.get('type'):
-                return
-
-            geom_type = str(geometry_data['type']).capitalize()
+            geom_type = geometry_data['type'].capitalize()
 
             if geom_type == VisualGeometry.Mesh.value:
-                mesh_file = visuals_data['geometry'].get('path')
-                if not mesh_file:
-                    raise ValueError(f"Mesh file {str(mesh_file)} is not a valid file")
-
+                mesh_file = geometry_data['path']
                 mesh = read_3d_model(pathlib.Path(path).joinpath(mesh_file).as_posix())
-
             elif geom_type == VisualGeometry.Box.value:
-                size = geometry_data.get('size')
-                if not isinstance(size, list) or len(size) != 3:
-                    raise ValueError("Invalid box size: must be a float array of length 3")
-
-                if any(val <= 0 for val in size):
-                    raise ValueError("Cannot have a box dimension size less than or equal to zero")
-
-                x, y, z = size
+                x, y, z = geometry_data['size']
                 mesh = create_cuboid(x, z, y)
-
             elif geom_type == VisualGeometry.Plane.value:
-                size = geometry_data.get('size')
-                if not isinstance(size, list) or len(size) != 3:
-                    raise ValueError("Invalid plane size: must be a float array of length 2")
-
-                if any(val <= 0 for val in size):
-                    raise ValueError("Cannot have a plane dimension size less than or equal to zero")
-
-                x, y = size
+                x, y = geometry_data['size']
                 mesh = create_plane(Plane.fromCoefficient(1, 1, 0, 0), x, y)
-
             elif geom_type == VisualGeometry.Sphere.value:
-                radius = geometry_data.get('radius')
-                if not isinstance(radius, float):
-                    raise ValueError("Invalid sphere radius: must be a float")
-
-                if radius <= 0:
-                    raise ValueError("Cannot have a sphere of radius less than or equal to zero")
-
+                radius = geometry_data['radius']
                 mesh = create_sphere(radius)
-
-            else:
-                return
 
         mesh.transform(pose)
         mesh.colour = Colour(*mesh_colour)
