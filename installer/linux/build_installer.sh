68331a8ae0ed78f3ff409841cc0299c7d1231525
Stephen Nneji <steve.nneji@gmail.com>
2023-10-16 11:50:46 +0100

    Adds a Github workflow to build installers for all supported OSes

--- installer/linux/build_installer.sh
+++ installer/linux/build_installer.sh
@@ -16,6 +16,11 @@ case $key in
     shift # past argument
     shift # past value
     ;;
+    -d|--build-dir)
+    BUILD_DIR=$(realpath "$2")
+    shift # past argument
+    shift # past value
+    ;;
     -r|--remote)
     REMOTE=YES
     shift # past argument
@@ -34,17 +39,18 @@ done
 if [ -n "$HELP" ]; then
   echo "Usage: build_installer [options]"
   echo "Options:"
-  echo "-h, --help		Show this help message and exit"
-  echo "-l <dir>, --local <dir>	Clone SScanSS 2 from local directory (requires git)"
-  echo "-t <arg>, --tag <arg>	Clone specific tag of SScanSS 2 from local (requires git) or web"
-  echo "-r, --remote		Clone SScanSS 2 from Github repo"
+  echo "-h, --help		     Show this help message and exit"
+  echo "-l <dir>, --local <dir>	     Clone SScanSS 2 from local directory (requires git)"
+  echo "-d <dir>, --build-dir <dir>  Specify build directory (temp directory will be used if not provided)"
+  echo "-t <arg>, --tag <arg>	     Clone specific tag of SScanSS 2 from local (requires git) or web"
+  echo "-r, --remote		     Clone SScanSS 2 from Github repo"
   exit 0
 fi
 
 
-command -v makeself >/dev/null 2>&1 || { 
-  echo >&2 "makeself is required but not installed"; 
-  exit 1; 
+command -v makeself >/dev/null 2>&1 || {
+  echo >&2 "makeself is required but not installed";
+  exit 1;
 }
 
 # trap ctrl-c and call finish()
@@ -58,8 +64,19 @@ SRC_DIR=$(dirname "$(realpath "$0")")
 
 echo ""
 echo "SScanSS 2 Installer Builder"
-echo ""	
-TMP_DIR=$(mktemp -d)
+echo ""
+
+if [ -n "$BUILD_DIR" ]; then
+  if [ "$(ls -A $BUILD_DIR)" ]; then
+    echo >&2 "error: the build directory '$BUILD_DIR' must be empty"
+    exit 1
+  else
+    TMP_DIR="$BUILD_DIR/build"
+    mkdir "$TMP_DIR"
+  fi
+else
+  TMP_DIR=$(mktemp -d)
+fi
 cd "$TMP_DIR"
 mkdir "$TMP_DIR/sscanss"
 
@@ -70,14 +87,14 @@ if [ -n "$REMOTE" ]; then
   else
     SSCANSS_URL="https://github.com/ISISNeutronMuon/SScanSS-2/tarball/master"
   fi
-  
+
   wget $SSCANSS_URL -O "$TMP_DIR/sscanss.tar.gz"
   tar xzf "$TMP_DIR/sscanss.tar.gz" -C "$TMP_DIR/sscanss" --strip-components=1
 
 elif [ -n "$LOCAL" ]; then
-  command -v git >/dev/null 2>&1 || { 
-    echo >&2 "git is required with the --local option."; 
-    exit 1; 
+  command -v git >/dev/null 2>&1 || {
+    echo >&2 "git is required with the --local option.";
+    exit 1;
   }
   echo "Cloning SScanSS 2 from local directory"
   if [ -n "$TAG" ]; then
@@ -127,8 +144,7 @@ if [ -n "$TAG" ]; then
   EXECUTABLE="SScanSS-2-${TAG:1}-installer.run"
 fi
 
-makeself --tar-extra "--exclude=.git --exclude=sscanss/3rdparty --exclude=sscanss/docsrc --exclude=sscanss/docs" "$STAGE_DIR" "$EXECUTABLE" "SScanSS-2 installer" ./sscanss/installer/linux/install.sh
-
+makeself --tar-format "posix" --tar-extra "--exclude=.git --exclude=sscanss/3rdparty --exclude=sscanss/docsrc --exclude=sscanss/docs" "$STAGE_DIR" "$EXECUTABLE" "SScanSS-2 installer" ./sscanss/installer/linux/install.sh
 cp -a "$EXECUTABLE" "$SRC_DIR/$EXECUTABLE"
 
 exit 0
